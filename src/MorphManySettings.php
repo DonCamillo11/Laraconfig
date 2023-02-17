cec415588367768d234c2ee2a361984b89218ca9
DonCamillo11 <125701444+DonCamillo11@users.noreply.github.com>
2023-02-17 22:38:11 +0100

    Update MorphManySettings.php

--- src/MorphManySettings.php
+++ src/MorphManySettings.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace DarkGhostHunter\Laraconfig;
+namespace DonCamillo1\Laraconfig;
 
-use DarkGhostHunter\Laraconfig\Eloquent\Metadata;
+use DonCamillo1\Laraconfig\Eloquent\Metadata;
 use Illuminate\Contracts\Cache\Factory;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
@@ -12,7 +12,7 @@ use Illuminate\Support\Arr;
 
 /**
  * @mixin \Illuminate\Database\Eloquent\Builder
- * @mixin \DarkGhostHunter\Laraconfig\Eloquent\Setting
+ * @mixin \DonCamillo1\Laraconfig\Eloquent\Setting
  *
  * @method Model|HasConfig getParent()
  */
@@ -28,7 +28,7 @@ class MorphManySettings extends MorphMany
     /**
      * The settings cache helper.
      *
-     * @var \DarkGhostHunter\Laraconfig\SettingsCache|null
+     * @var \DonCamillo1\Laraconfig\SettingsCache|null
      */
     protected ?SettingsCache $cache = null;
 
