4f9015b167252a30cadbb0f5ec072db9af84aa09
DonCamillo11 <125701444+DonCamillo11@users.noreply.github.com>
2023-02-17 22:53:55 +0100

    Update SettingRegistrar.php

--- src/Registrar/SettingRegistrar.php
+++ src/Registrar/SettingRegistrar.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace DarkGhostHunter\Laraconfig\Registrar;
+namespace DonCamillo1\Laraconfig\Registrar;
 
 use Illuminate\Contracts\Config\Repository;
 use Illuminate\Contracts\Foundation\Application;
